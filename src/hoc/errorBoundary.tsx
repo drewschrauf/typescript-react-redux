19bfaceb16594738fd56dbc4109a335e94a2fc2c
Drew Schrauf <andrew.schrauf@gmail.com>
2019-01-31 21:38:21 +1100

    Fix up tests and style ErrorMessage

--- src/hoc/errorBoundary.tsx
+++ src/hoc/errorBoundary.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 
 type ReactComponent<T> = React.ComponentClass<T> | React.StatelessComponent<T>;
+
+const ErrorMessage = styled.div`
+  border: 1px solid red;
+  padding: 10px;
+`;
 
 const errorBoundary = <T extends {}>(Component: ReactComponent<T>) => {
   return class ErrorBoundary extends React.Component<T, { hasError: boolean }> {
@@ -21,7 +27,11 @@ const errorBoundary = <T extends {}>(Component: ReactComponent<T>) => {
 
     render() {
       const { hasError } = this.state;
-      return !hasError ? <Component {...this.props} /> : <div>Something went wrong</div>;
+      return !hasError ? (
+        <Component {...this.props} />
+      ) : (
+        <ErrorMessage>Something went wrong</ErrorMessage>
+      );
     }
   };
 };
