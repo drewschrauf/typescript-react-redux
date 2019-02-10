59606193c609c0f614713a9684c932d5b0894381
Drew Schrauf <andrew.schrauf@gmail.com>
2019-02-10 14:35:43 +1100

    Remove unused dependencies

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -10,23 +10,19 @@ const DEFAULT_PROPS = { match: { params: {} } } as any;
 
 jest.useFakeTimers();
 
+const renderWithProvider = (element: React.ReactNode) => {
+  return render(<Provider store={store()}>{element}</Provider>);
+};
+
 describe('counter', () => {
   it('should render current count', () => {
-    const root = render(
-      <Provider store={store()}>
-        <Counter {...DEFAULT_PROPS} />
-      </Provider>,
-    );
+    const root = renderWithProvider(<Counter {...DEFAULT_PROPS} />);
 
     expect(root.getByText('Count 0')).toBeInTheDocument();
   });
 
   it('should increment amount when increment button clicked', () => {
-    const root = render(
-      <Provider store={store()}>
-        <Counter {...DEFAULT_PROPS} />
-      </Provider>,
-    );
+    const root = renderWithProvider(<Counter {...DEFAULT_PROPS} />);
 
     fireEvent.click(root.getByText('Increment by 1'));
 
@@ -34,11 +30,7 @@ describe('counter', () => {
   });
 
   it('should decrement amount when decrement button clicked', () => {
-    const root = render(
-      <Provider store={store()}>
-        <Counter {...DEFAULT_PROPS} />
-      </Provider>,
-    );
+    const root = renderWithProvider(<Counter {...DEFAULT_PROPS} />);
 
     fireEvent.click(root.getByText('Decrement by 1'));
 
@@ -46,11 +38,7 @@ describe('counter', () => {
   });
 
   it('should delay increment amount when increment button clicked', async () => {
-    const root = render(
-      <Provider store={store()}>
-        <Counter {...DEFAULT_PROPS} />
-      </Provider>,
-    );
+    const root = renderWithProvider(<Counter {...DEFAULT_PROPS} />);
 
     fireEvent.click(root.getByText('Delayed increment by 1'));
     expect(root.getByText('Count 0')).toBeInTheDocument();
@@ -63,10 +51,8 @@ describe('counter', () => {
   });
 
   it('should increment by given amount when route provides increment amount', () => {
-    const root = render(
-      <Provider store={store()}>
-        <Counter {...set(['match', 'params', 'by'], '7', DEFAULT_PROPS)} />
-      </Provider>,
+    const root = renderWithProvider(
+      <Counter {...set(['match', 'params', 'by'], '7', DEFAULT_PROPS)} />,
     );
 
     fireEvent.click(root.getByText('Increment by 7'));
@@ -75,10 +61,8 @@ describe('counter', () => {
   });
 
   it('should decrement by given amount when route provides decrement amount', () => {
-    const root = render(
-      <Provider store={store()}>
-        <Counter {...set(['match', 'params', 'by'], '7', DEFAULT_PROPS)} />
-      </Provider>,
+    const root = renderWithProvider(
+      <Counter {...set(['match', 'params', 'by'], '7', DEFAULT_PROPS)} />,
     );
 
     fireEvent.click(root.getByText('Decrement by 7'));
@@ -87,10 +71,8 @@ describe('counter', () => {
   });
 
   it('should delay increment by given amount when route provides increment amount', async () => {
-    const root = render(
-      <Provider store={store()}>
-        <Counter {...set(['match', 'params', 'by'], '7', DEFAULT_PROPS)} />
-      </Provider>,
+    const root = renderWithProvider(
+      <Counter {...set(['match', 'params', 'by'], '7', DEFAULT_PROPS)} />,
     );
 
     fireEvent.click(root.getByText('Delayed increment by 7'));
