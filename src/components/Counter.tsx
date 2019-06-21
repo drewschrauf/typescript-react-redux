52f9de55a4bfa301298b1e3da2c6c932c1a1d644
Drew Schrauf <andrew.schrauf@gmail.com>
2019-06-21 23:31:50 +1000

    Switch redux implementation to use hooks

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import styled from 'styled-components';
 
 import { State, Dispatch } from '@/store';
@@ -8,19 +8,6 @@ import { decrementBy, delayedIncrementBy, incrementBy } from '@/store/actions/co
 interface CounterProps {
   /** The amount to increment the counter by on each click */
   readonly incrementAmount: number;
-}
-
-interface ConnectedProps {
-  /** The current count */
-  readonly count: number;
-  /** Is a delayed increment currently being performed? */
-  readonly pending: boolean;
-  /** Action to perform when clicking increment button */
-  readonly increment: () => void;
-  /** Action to perform when clicking decrement button */
-  readonly decrement: () => void;
-  /** Action to perform when clicking delayed increment button */
-  readonly delayedIncrement: () => void;
 }
 
 const Wrapper = styled.div`
@@ -41,37 +28,24 @@ const Button = styled.button`
   }
 `;
 
-const CounterComponent = ({
-  incrementAmount,
-  count,
-  increment,
-  decrement,
-  pending,
-  delayedIncrement,
-}: CounterProps & ConnectedProps) => (
-  <Wrapper>
-    <h1>Count {count}</h1>
-    <Button onClick={increment}>Increment by {incrementAmount}</Button>
-    <Button onClick={decrement}>Decrement by {incrementAmount}</Button>
-    <Button disabled={pending} onClick={delayedIncrement}>
-      Delayed increment by {incrementAmount}
-    </Button>
-  </Wrapper>
-);
-
 /**
  * React component that renders the main UI. It displays the current count and provides buttons
  * for modifying it.
  */
-const Counter = connect(
-  (state: State) => ({
-    count: state.counter.count,
-    pending: state.counter.pending,
-  }),
-  (dispatch: Dispatch, ownProps: CounterProps) => ({
-    decrement: () => dispatch(decrementBy({ amount: ownProps.incrementAmount })),
-    delayedIncrement: () => dispatch(delayedIncrementBy({ amount: ownProps.incrementAmount })),
-    increment: () => dispatch(incrementBy({ amount: ownProps.incrementAmount })),
-  }),
-)(CounterComponent) as React.ComponentClass<CounterProps>;
+const Counter = ({ incrementAmount: amount }: CounterProps) => {
+  const count = useSelector((state: State) => state.counter.count);
+  const pending = useSelector((state: State) => state.counter.pending);
+  const dispatch = useDispatch<Dispatch>();
+  return (
+    <Wrapper>
+      <h1>Count {count}</h1>
+      <Button onClick={() => dispatch(incrementBy({ amount }))}>Increment by {amount}</Button>
+      <Button onClick={() => dispatch(decrementBy({ amount }))}>Decrement by {amount}</Button>
+      <Button disabled={pending} onClick={() => dispatch(delayedIncrementBy({ amount }))}>
+        Delayed increment by {amount}
+      </Button>
+    </Wrapper>
+  );
+};
+
 export default Counter;
